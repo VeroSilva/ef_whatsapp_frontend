183b37fafe8b9e1936c5b933bce979d562eec228
Verónica Silva Barreto <veronicasilvabarreto@Veronicas-MacBook-Pro.local>
2023-05-29 12:15:42 -0500

    feat: Chat page

---
+++ src/app/pages/page.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+function HomePage() {
+    return (
+      <div>
+        <h1>Página de inicio</h1>
+        <p>Bienvenido al chat de Whatsapp de EF Perfumes</p>
+        <Link href="./pages/login">Login</Link>
+        <Link href="./pages/chat">Chat</Link>
+      </div>
+    );
+  }
+  
+export default HomePage;
