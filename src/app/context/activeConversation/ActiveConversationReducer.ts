a4d3bb8d478a8f7f380bde4dd98f6b714ac589e2
Verónica Silva Barreto <veronicasilvabarreto@Veronicas-MacBook-Pro.local>
2023-07-01 16:12:44 -0500

    refactor: Reset active conversation

--- src/app/context/activeConversation/ActiveConversationReducer.ts
+++ src/app/context/activeConversation/ActiveConversationReducer.ts
@@ -10,7 +10,7 @@ export const ActiveConversationReducer = (state: ContainerActiveConversation, ac
       window.localStorage.setItem(ACTIVE_CONVERSATION, JSON.stringify(action.payload))
       return action.payload
     case 'resetActiveConversation':
-      window.localStorage.clear();
+      window.localStorage.removeItem(ACTIVE_CONVERSATION)
       return INITIAL_STATE
     default:
       return state
