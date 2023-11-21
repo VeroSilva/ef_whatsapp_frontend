b08600078d894d2177cd5f0f679d08652d69be02
Verónica Silva Barreto <veronicasilvabarreto@Veronicas-MacBook-Pro.local>
2023-11-21 13:15:03 -0500

    refactor: Interactive data name

--- src/app/components/Message/MessageContent/MessageOrder/MessageOrder.tsx
+++ src/app/components/Message/MessageContent/MessageOrder/MessageOrder.tsx
@@ -10,8 +10,9 @@ export const MessageOrder = ({ message }: { message: any }) => {
                     {json.product_items.map((product: any, index: number) => (
                         <div key={`product-${index}`} className="mb-2">
                             <h3 className="font-semibold">Producto {index + 1}</h3>
-                            <p><span className="text-teal-400">ID:</span> {product.product_retailer_id}</p>
-                            <p><span className="text-teal-400">Precio:</span> {product.currency} {product.item_price}</p>
+                            <p><span className="text-teal-400">Nombre:</span> {product.title}</p>
+                            {/* <p><span className="text-teal-400">ID:</span> {product.product_retailer_id}</p> */}
+                            {/* <p><span className="text-teal-400">Precio:</span> {product.currency} {product.item_price}</p> */}
                             <p><span className="text-teal-400">Cantidad:</span> {product.quantity}</p>
                         </div>
                     ))}
