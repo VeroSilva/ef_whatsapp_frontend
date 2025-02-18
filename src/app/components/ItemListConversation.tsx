aebb5e819274e5ad4f19d11a23bdc55ae29a2e6c
Verónica Silva Barreto <veronicasilvabarreto@Veronicas-MacBook-Pro.local>
2025-02-17 22:34:29 -0500

    refactor: item chat font size

--- src/app/components/ItemListConversation.tsx
+++ src/app/components/ItemListConversation.tsx
@@ -105,7 +105,7 @@ export const ItemListConversation = ({ conversation, handleOpenConversation, act
                     {
                         Math.floor((Date.now() - conversation.message_created_at * 1000) / (1000 * 60 * 60)) >= 24 && (
                             <div className="group relative">
-                                <IconWarning classes="w-5 h-5 text-yellow-400 inline-block me-1" />
+                                <IconWarning classes="w-4 h-4 text-yellow-400 inline-block me-1" />
                                 <span className="z-50 whitespace-nowrap absolute top- right-0 scale-0 transition-all rounded bg-gray-800 p-2 text-xs text-white group-hover:scale-100">Recontactar cliente</span>
                             </div>
                         )
@@ -114,12 +114,12 @@ export const ItemListConversation = ({ conversation, handleOpenConversation, act
             </div>
 
             <div className="mx-2 overflow-hidden flex-1">
-                <a href="#" className="text-xs font-bold overflow-hidden text-slate-800 overflow-ellipsis whitespace-nowrap">
+                <a href="#" className="text-[14px] font-bold overflow-hidden text-slate-800 overflow-ellipsis whitespace-nowrap">
                     {highlightText(conversation.contact.name, filter)}
                 </a>
 
                 {conversation.phone !== null &&
-                    <div className="w-full truncate text-xs text-slate-800">
+                    <div className="w-full truncate text-[14px] text-slate-800">
                         {highlightText(formatPhoneNumber(conversation.contact.phone), filter)}
                     </div>
                 }
@@ -128,39 +128,39 @@ export const ItemListConversation = ({ conversation, handleOpenConversation, act
                     <div className="flex-1 truncate">
                         {conversation.status !== "client" ? (
                             conversation.status === "trying" ?
-                                <IconClock classes="w-5 h-5 text-slate-500 inline-block me-1" /> :
+                                <IconClock classes="w-4 h-4 text-slate-500 inline-block me-1" /> :
                                 conversation.status === "sent" ?
-                                    <IconCheck classes="w-5 h-5 text-slate-500 inline-block me-1" /> :
+                                    <IconCheck classes="w-4 h-4 text-slate-500 inline-block me-1" /> :
                                     conversation.status === "delivered" ?
-                                        <IconDoubleCheck classes="w-5 h-5 text-slate-500 inline-block me-1" /> :
+                                        <IconDoubleCheck classes="w-4 h-4 text-slate-500 inline-block me-1" /> :
                                         conversation.status === "read" ?
-                                            <IconDoubleCheck classes="w-5 h-5 text-cyan-500 inline-block me-1" /> :
-                                            <IconExclamationCircle classes="w-5 h-5 text-red-500 inline-block me-1" />
+                                            <IconDoubleCheck classes="w-4 h-4 text-cyan-500 inline-block me-1" /> :
+                                            <IconExclamationCircle classes="w-4 h-4 text-red-500 inline-block me-1" />
                         ) : null
                         }
 
                         {conversation.message_type === "text" ?
-                            <span className="text-[14px] text-slate-500">{conversation.last_message}</span> :
+                            <span className="text-[12px] text-slate-500">{conversation.last_message}</span> :
                             conversation.message_type === "audio" ?
-                                <span className="text-[14px] text-slate-500"><IconMicrophone classes="w-4 h-4 text-slate-500 inline-block" /> Audio</span> :
+                                <span className="text-[12px] text-slate-500"><IconMicrophone classes="w-4 h-4 text-slate-500 inline-block" /> Audio</span> :
                                 conversation.message_type === "video" ?
-                                    <span className="text-[14px] text-slate-500"><IconVideoCamera classes="w-4 h-4 text-slate-500 inline-block" /> Video</span> :
+                                    <span className="text-[12px] text-slate-500"><IconVideoCamera classes="w-4 h-4 text-slate-500 inline-block" /> Video</span> :
                                     conversation.message_type === "document" ?
-                                        <span className="text-[14px] text-slate-500"><IconDocument classes="w-4 h-4 text-slate-500 inline-block" /> Documento</span> :
+                                        <span className="text-[12px] text-slate-500"><IconDocument classes="w-4 h-4 text-slate-500 inline-block" /> Documento</span> :
                                         conversation.message_type === "image" ?
-                                            <span className="text-[14px] text-slate-500"><IconImage classes="w-4 h-4 text-slate-500 inline-block" /> Imagen</span> :
+                                            <span className="text-[12px] text-slate-500"><IconImage classes="w-4 h-4 text-slate-500 inline-block" /> Imagen</span> :
                                             conversation.message_type === "sticker" ?
-                                                <span className="text-[14px] text-slate-500"><IconSticker classes="w-4 h-4 text-slate-500 inline-block" /> Sticker</span> :
+                                                <span className="text-[12px] text-slate-500"><IconSticker classes="w-4 h-4 text-slate-500 inline-block" /> Sticker</span> :
                                                 conversation.message_type === "reaction" ?
                                                     (conversation.status === "client" ? "Reaccionó " : "Reaccionaste ") + conversation.last_message + " a un mensaje" :
                                                     conversation.message_type === "location" ?
-                                                        <span className="text-[14px] text-slate-500"><IconLocation classes="w-4 h-4 text-slate-500 inline-block" /> Ubicación</span> :
+                                                        <span className="text-[12px] text-slate-500"><IconLocation classes="w-4 h-4 text-slate-500 inline-block" /> Ubicación</span> :
                                                         conversation.message_type === "template" ?
-                                                            <span className="text-[14px] text-slate-500"><IconMessages classes='w-4 h-4 text-slate-500 inline-block' /> Template</span> :
+                                                            <span className="text-[12px] text-slate-500"><IconMessages classes='w-4 h-4 text-slate-500 inline-block' /> Template</span> :
                                                             conversation.message_type === "button" ?
-                                                                <span className="text-[14px] text-slate-500"><IconReply classes='w-4 h-4 text-slate-500 inline-block' /> Respuesta template</span> :
+                                                                <span className="text-[12px] text-slate-500"><IconReply classes='w-4 h-4 text-slate-500 inline-block' /> Respuesta template</span> :
                                                                 conversation.message_type === "interactive" ?
-                                                                    <span className="text-[14px] text-slate-500">Productos</span> :
+                                                                    <span className="text-[12px] text-slate-500">Productos</span> :
                                                                     <></>
                             //falta contact 
                         }
