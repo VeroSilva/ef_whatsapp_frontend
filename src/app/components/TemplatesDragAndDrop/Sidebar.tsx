f7f92154446113a9be61c98588fe7347862553bf
Verónica Silva Barreto <veronicasilvabarreto@Veronicas-MacBook-Pro.local>
2025-02-21 22:43:43 -0500

    refactor: sidebar flow accordions and searchable templates

--- src/app/components/TemplatesDragAndDrop/Sidebar.tsx
+++ src/app/components/TemplatesDragAndDrop/Sidebar.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, ChangeEvent, DragEvent } from 'react';
 import useTemplates from "../../hooks/useTemplates";
 import { IconLoading } from '../Icons/IconLoading';
 import useUser from "../../hooks/useUser"
@@ -8,19 +8,22 @@ import { importTemplates } from '@/app/services/api';
 import { IconCheckCircle } from '../Icons/IconCheckCircle';
 import { IconInfo } from '../Icons/IconInfo';
 import useActivePhone from "@/app//hooks/useActivePhone";
+import { Accordion } from '../Accordion/Accordion';
+import { IconSearch } from '../Icons/IconSearch';
 
 const Sidebar = ({ handleSaveFlow, loadingSave }: { handleSaveFlow: () => void, loadingSave: boolean }) => {
     const { templatesState } = useTemplates();
     const { userState } = useUser();
     const { activePhone } = useActivePhone();
-    const [loadingImport, setLoadingImport] = useState(false)
+    const [filteredTemplates, setFilteredTemplates] = useState(templatesState);
+    const [loadingImport, setLoadingImport] = useState(false);
     const [alert, setAlert] = useState({
         type: "",
         message: "",
         show: false
     });
 
-    const onDragStart = (event: React.DragEvent<HTMLDivElement>, nodeType: string, template?: any) => {
+    const onDragStart = (event: DragEvent<HTMLDivElement>, nodeType: string, template?: any) => {
         event.dataTransfer.setData('application/reactflow', nodeType);
         event.dataTransfer.effectAllowed = 'move';
 
@@ -40,6 +43,15 @@ const Sidebar = ({ handleSaveFlow, loadingSave }: { handleSaveFlow: () => void, 
         })
     }
 
+    const handleSearchChange = (event: ChangeEvent<HTMLInputElement>) => {
+        const { value } = event.target;
+        const currentSearch = templatesState.filter(template =>
+            template.name.toLowerCase().startsWith(value)
+        )
+    
+        setFilteredTemplates(currentSearch)
+    };
+
     useEffect(() => {
         if (alert.show) {
             setTimeout(() => {
@@ -52,29 +64,61 @@ const Sidebar = ({ handleSaveFlow, loadingSave }: { handleSaveFlow: () => void, 
         }
     }, [alert])
 
+    useEffect(() => {
+      if (templatesState) setFilteredTemplates(templatesState)
+    }, [templatesState])
+
     return (
         <>
             <aside className="flex flex-col justify-between">
-                <div className='overflow-y-auto'>
-                    {templatesState &&
-                        templatesState.map((template, index) => {
-                            return (
-                                <div key={index} className="dndnode" onDragStart={(event) => onDragStart(event, 'templateNode', template)} draggable>
-                                    {template.name}
-                                </div>
-                            )
-                        })
-                    }
+                <div className='flex flex-col gap-2'>
+                    <Accordion
+                        title="Básicos"
+                        titleClassname="block uppercase tracking-wide text-gray-700 text-xs text-left font-bold"
+                        defaultIsOpen={true}
+                    >
+                        <div className="dndnode" onDragStart={(event) => onDragStart(event, 'textNode')} draggable>Texto</div>
 
-                    <div className="dndnode" onDragStart={(event) => onDragStart(event, 'textNode')} draggable>Texto</div>
+                        <div className="dndnode" onDragStart={(event) => onDragStart(event, 'imageNode')} draggable>Imagen</div>
 
-                    <div className="dndnode" onDragStart={(event) => onDragStart(event, 'imageNode')} draggable>Imagen</div>
+                        <div className="dndnode" onDragStart={(event) => onDragStart(event, 'videoNode')} draggable>Vídeo</div>
 
-                    <div className="dndnode" onDragStart={(event) => onDragStart(event, 'videoNode')} draggable>Vídeo</div>
+                        <div className="dndnode" onDragStart={(event) => onDragStart(event, 'audioNode')} draggable>Audio</div>
 
-                    <div className="dndnode" onDragStart={(event) => onDragStart(event, 'audioNode')} draggable>Audio</div>
+                        <div className="dndnode" onDragStart={(event) => onDragStart(event, 'interactiveNode')} draggable>Interactivo</div>
+                    </Accordion>
 
-                    <div className="dndnode" onDragStart={(event) => onDragStart(event, 'interactiveNode')} draggable>Interactivo</div>
+                    <Accordion
+                        title="Templates"
+                        titleClassname="block uppercase tracking-wide text-gray-700 text-xs text-left font-bold"
+                        defaultIsOpen={false}
+                    >
+                        <div className="relative">
+                            <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                                <IconSearch classes="w-5 h-5 absolute inset-y-0 left-0 my-auto text-slate-400 ml-3" />
+                            </div>
+                            <input
+                                type="search"
+                                id="default-search"
+                                className="block w-full p-2 pl-10 mb-2 text-xs text-gray-900 border border-gray-300 rounded-lg bg-gray-50 input-sky"
+                                placeholder="Buscar template"
+                                required
+                                onChange={handleSearchChange}
+                            />
+                        </div>
+
+                        <div className="max-h-80 overflow-y-auto">
+                            {filteredTemplates &&
+                                filteredTemplates.map((template, index) => {
+                                    return (
+                                        <div key={index} className="dndnode" onDragStart={(event) => onDragStart(event, 'templateNode', template)} draggable>
+                                            {template.name}
+                                        </div>
+                                    )
+                                })
+                            }
+                        </div>
+                    </Accordion>
                 </div>
 
                 <div className="actions-container flex flex-col	gap-4">
